8e4b0d8bdf56f82dbee204212036ef63b6f61a27
KimBamYa <recon7347@gmail.com>
2020-05-11 00:03:20 +0900

    마커가 제대로 찍히지 않는 문제 해결

--- project/src/JFXTest.java
+++ project/src/JFXTest.java
@@ -40,19 +40,26 @@ public class JFXTest extends JFrame {
 		group.getChildren().add(webView);
 		webView.setMinSize(500, 500);
 		webView.setMaxSize(500, 500);
-
-		WebEngine webEngine = webView.getEngine();
-		//웹서버
-		webEngine.load("https://dean7347.github.io/BistroMap/map");
-		//로컬
-		//webEngine.load("http://127.0.0.1:5500/map.html");			
-		//로딩체크
-		System.out.println(webEngine.getLoadWorker().stateProperty());
 		
+		WebEngine webEngine = webView.getEngine();
+		webEngine.setJavaScriptEnabled(true);
+		//웹서버
+		//webEngine.load("https://dean7347.github.io/BistroMap/map");
+		//로컬
+		webEngine.load("http://127.0.0.1:5500/map.html");
+		webEngine.reload();
+			//webEngine.executeScript("markPosition('ㅇㄹ번째',33.953705, 126.570677)");
+			//webEngine.executeScript("test()");
+		
+		
+		//테스트
+		//webEngine.load("https://naver.com");
+		//로딩체크
+		//System.out.println(webEngine.getLoadWorker().stateProperty());
 		
 	
 		
-		webEngine.getLoadWorker().stateProperty().addListener(
+		 	webEngine.getLoadWorker().stateProperty().addListener(
 			    new ChangeListener() {
 			        @Override
 			        public void changed(ObservableValue observable, Object oldValue, Object newValue) {
@@ -60,15 +67,32 @@ public class JFXTest extends JFrame {
 			            System.out.println("newValue: " + newValue);
 
 			            if (newValue != Worker.State.SUCCEEDED) {
+		
 			                return;
 			            }
-			            // markPosiont 찍어주는 함수입니다
-			            webEngine.executeScript("markPosition('두번째',33.453705, 126.570677)");
 			            
+			            
+			            
+			            if (newValue == Worker.State.SUCCEEDED) {
+			                //markPosiont 찍어주는 함수입니다
+			            	//webEngine.executeScript("markPosition('ㅇㄹ번째',33.953705, 126.570677)");
+			            	webEngine.executeScript("insertMarkInfo('카카오',33.450705, 126.570677)");
+			            	webEngine.executeScript("insertMarkInfo('생태연못',33.450936, 126.569477)");
+			            	webEngine.executeScript("mark()");
+			            	
+			                return;
+			            }
+			            webEngine.reload();
+
+			           
 			        
 			        }
 			    }
 			);
+		 
+		
+
+		
 		
 	
 		           
@@ -77,5 +101,7 @@ public class JFXTest extends JFrame {
 	
 	public static void main(String[] args) {
 		new JFXTest();
+		
+		
 	}
 }
