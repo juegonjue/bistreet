beee52fa97dd397f9f21cf70559369c468e05921
KimBamYa <recon7347@gmail.com>
2020-06-03 05:29:15 +0900

    검색기능추가

--- project/bistroFX/src/application/NearRecomController.java
+++ project/bistroFX/src/application/NearRecomController.java
@@ -1,4 +1,5 @@
 package application;
+
 import java.net.URL;
 import java.util.ResourceBundle;
  
@@ -14,6 +15,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.util.Duration;
@@ -23,7 +25,9 @@ import javafx.scene.web.WebView;
  
 public class NearRecomController implements Initializable {
    @FXML private Button btnMain;
-    
+   @FXML private Button btnSearch;
+   @FXML private Button btnpick;
+   @FXML private TextField textAddress;
   @FXML private WebView webView;
   private WebEngine webEngine; 
 
@@ -31,17 +35,19 @@ public class NearRecomController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
        btnMain.setOnAction(e->handleBtnLogin(e));
-        
+       btnSearch.setOnAction(e->handleBtns(e));
+       btnpick.setOnAction(e->handlebtnpick(e));
        webEngine = webView.getEngine();
      webEngine.load("https://dean7347.github.io/BistroMap/map");
      //webEngine.load("https://google.com");
+     //webEngine.load("http://127.0.0.1:5500/map.html");
 	 	webEngine.getLoadWorker().stateProperty().addListener(
 			    new ChangeListener() {
 			        @Override
 			        public void changed(ObservableValue observable, Object oldValue, Object newValue) {
 			            System.out.println("oldValue: " + oldValue);
 			            System.out.println("newValue: " + newValue);
-
+			            
 			            if (newValue != Worker.State.SUCCEEDED) {
 		
 			                return;
@@ -52,9 +58,10 @@ public class NearRecomController implements Initializable {
 			            if (newValue == Worker.State.SUCCEEDED) {
 			                //markPosiont 찍어주는 함수입니다
 			            	//webEngine.executeScript("markPosition('ㅇㄹ번째',33.953705, 126.570677)");
-			            	webEngine.executeScript("insertMarkInfo('카카오',33.450705, 126.570677)");
-			            	webEngine.executeScript("insertMarkInfo('생태연못',33.450936, 126.569477)");
-			            	webEngine.executeScript("mark()");
+			            	//webEngine.executeScript("insertMarkInfo('카카오',33.450705, 126.570677)");
+			            	//webEngine.executeScript("insertMarkInfo('생태연못',33.450936, 126.569477)");
+			            	//webEngine.executeScript("mark()");
+			            	//System.out.println(webEngine.executeScript("placexy()"));
 			            	
 			                return;
 			            }
@@ -81,6 +88,25 @@ public class NearRecomController implements Initializable {
         {
             e.printStackTrace();
         }
+    }
+    
+    
+    
+    public void  handleBtns(ActionEvent event) 
+    {
+    	String address = textAddress.getText();
+    	System.out.println(address);
+    	String search="addrsearch("+"'"+address+"'"+")";
+    	webEngine.executeScript(search);
+    	
+    }
+    
+    public void  handlebtnpick(ActionEvent event) 
+    {
+    	System.out.println(webEngine.executeScript("pushX()"));
+    	System.out.println(webEngine.executeScript("pushY()"));
+    	
+    	
     }
     
     //new LoadMap();
