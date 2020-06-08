b23effb6b966ee3d5dcfb341083e6a63341cf65c
KimBamYa <recon7347@gmail.com>
2020-06-08 15:38:57 +0900

    랜덤 UI 컨트롤러 구현

--- project/bistroFX/src/application/MealRandomController.java
+++ project/bistroFX/src/application/MealRandomController.java
@@ -1,21 +1,33 @@
 package application;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 
 
 public class MealRandomController {
-@FXML Button btnmain;
+@FXML private Button btnmain;
+@FXML private Button btnok;
+@FXML private ComboBox comboBoxcat;
 
 
 
 public void initialize() {
 	
 	btnmain.setOnMouseClicked(e->App.go("main.fxml"));
-
+  	btnok.setOnAction(e->handlebtncat(e));
 	
 }
 	
+
+public void  handlebtncat(ActionEvent event) 
+{
+	
+	System.out.println(comboBoxcat.getValue());
+	
+	
+}
 	
 }
