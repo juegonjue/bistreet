c40e9832f6166b38a396225b291ece8b64b7e115
KimBamYa <recon7347@gmail.com>
2020-06-13 15:18:45 +0900

    간절한기도

--- project/bistroFX/src/application/MealController.java
+++ project/bistroFX/src/application/MealController.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class CategoryController {
+public class MealController {
 
 
     @FXML
