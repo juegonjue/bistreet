ff87a867350cf4f121d2702951cf324627430090
juegonjue <tojue123@naver.com>
2020-06-13 13:11:36 +0900

    mysql에 preparedStatement를 사용할 수 있는 함수 추가됨.  사용법은 디스코드에 업로드예정

---
+++ project/bistroFX/core/Mysql.java
@@ -0,0 +1,80 @@
+package core;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Mysql {
+
+	private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	private final String DB_URL = "jdbc:mysql://localhost/bistreet";
+	private final String ID = "root";
+	private final String PW = "password";
+	
+	private static Mysql obj = null;
+	private Connection conn = null;
+    private Statement st = null;
+    private ResultSet rs = null;
+    private String sql = null;
+    
+	public Mysql() throws SQLException {
+		try {
+			Class.forName(JDBC_DRIVER);
+			System.out.println(">>");
+			conn = DriverManager.getConnection(DB_URL, ID, PW);
+			System.out.print("DB접속 ");
+			if (conn!=null) System.out.println("성공");
+			else System.out.println("실패");
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Mysql.getConnection() -> DB연결
+	public static Mysql getConnection() throws SQLException {
+		if (Mysql.obj == null)
+			Mysql.obj = new Mysql();
+		System.out.println("getConnection : success");
+		return Mysql.obj;
+	}
+
+	//.sql("sql문 입력")
+	public void sql(String sql) throws SQLException {
+		this.sql = sql;
+		System.out.println("sql : success");
+	}
+	
+	public ResultSet select() throws SQLException {
+		st = conn.createStatement();
+		rs = st.executeQuery(sql);
+		System.out.println("resultSet : success (결과가 많을 시 여러 번 반복될 수 있습니다)");
+		return rs;
+	}
+	
+	//삽입, 반환값은 반영된 레코드수
+	public int insert() throws SQLException {
+		return st.executeUpdate(sql);
+	}
+	
+	//수정, 반환값은 반영된 레코드수
+	public int update() throws SQLException {
+		return st.executeUpdate(sql);
+	}
+
+	//삭제, 반환값은 반영된 레코드수	
+	public int delete() throws SQLException {
+		return st.executeUpdate(sql);
+	}
+	
+	public void close() throws SQLException {
+		if (rs != null)
+			rs.close();
+		if (st != null)
+			st.close();
+		if (conn != null)
+			conn.close();
+	}
+}
---
+++ project/bistroFX/core/StoreDAO.java
@@ -0,0 +1,93 @@
+package core;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class StoreDAO {
+
+	private ResultSet rs;
+	private String sql;
+	private ArrayList<Store> list;
+
+	public StoreDAO() {}
+
+	/*요식업소_식사조회(전체)*/ 
+	public ArrayList<Store> selectAllMeal() throws SQLException {
+		
+		rs = null; list = null;
+		Mysql mysql = Mysql.getConnection();	//호출	
+		sql = "select * from 요식업소 where 상권업종중분류코드 IN ('q01','q02','q03','q04','q05','q06','q07','q10','q13','q14')";
+		mysql.sql(sql);
+		rs = mysql.select();
+		
+		list = new ArrayList<Store>();
+		while(rs.next()) {
+			Integer storeNumber = rs.getInt("상가업소번호");
+			String storeName = rs.getString("상호명");
+			String storeLocation = rs.getString("지점명");
+			String codeMedium = rs.getString("상권업종중분류코드");
+			String codeSmall = rs.getString("상권업종소분류코드");
+			String storeAddress = rs.getString("도로명주소");
+			double kDegree = rs.getDouble("경도");
+			double wDegree = rs.getDouble("위도");
+			Store s = new Store(storeNumber,storeName,storeLocation,codeMedium,codeSmall,storeAddress,kDegree,wDegree);
+			list.add(s);
+		}		
+		return list;
+	}
+	
+	
+	/*요식업소_디저트조회(전체) -> 전체 불러와서 뿌려줄때 깔끔하게 , 이후에 ui 상황 보고 요구하는것 일부만 들고오기로 수정하기. 아니면 과부하 씨게 걸림*/
+	public ArrayList<Store> selectAllDessert() throws SQLException {
+		
+		rs = null; list = null;
+		Mysql mysql = Mysql.getConnection();	//호출	
+		sql = "select * from 요식업소 where 상권업종중분류코드 IN ('q08','q12')";
+		mysql.sql(sql);
+		rs = mysql.select();	
+
+		list = new ArrayList<Store>();
+		while(rs.next()) {
+			Integer storeNumber = rs.getInt("상가업소번호");
+			String storeName = rs.getString("상호명");
+			String storeLocation = rs.getString("지점명");
+			String codeMedium = rs.getString("상권업종중분류코드");
+			String codeSmall = rs.getString("상권업종소분류코드");
+			String storeAddress = rs.getString("도로명주소");
+			double kDegree = rs.getDouble("경도");
+			double wDegree = rs.getDouble("위도");
+			Store s = new Store(storeNumber,storeName,storeLocation,codeMedium,codeSmall,storeAddress,kDegree,wDegree);
+			list.add(s);
+		}
+		return list;
+	}
+	
+	
+	/*요식업소_주점조회(전체)*/ 
+	public ArrayList<Store> selectAllBeer() throws SQLException {
+		
+		rs = null; list = null;
+		Mysql mysql = Mysql.getConnection();	//호출	
+		sql = "select * from 요식업소 where 상권업종중분류코드='q09'";
+		mysql.sql(sql);
+		rs = mysql.select();
+		
+		list = new ArrayList<Store>();
+		while(rs.next()) {
+			Integer storeNumber = rs.getInt("상가업소번호");
+			String storeName = rs.getString("상호명");
+			String storeLocation = rs.getString("지점명");
+			String codeMedium = rs.getString("상권업종중분류코드");
+			String codeSmall = rs.getString("상권업종소분류코드");
+			String storeAddress = rs.getString("도로명주소");
+			double kDegree = rs.getDouble("경도");
+			double wDegree = rs.getDouble("위도");
+			Store s = new Store(storeNumber,storeName,storeLocation,codeMedium,codeSmall,storeAddress,kDegree,wDegree);
+			list.add(s);
+		}
+		return list;
+	}
+	
+	 
+}
